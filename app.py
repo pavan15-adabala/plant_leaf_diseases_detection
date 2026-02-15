62baa8098e7a1850eb077de65843640d84f4d633
pavan15-adabala <pavanadabala15@gmail.com>
2026-02-15 15:37:35 +0530

    Render deployment config

--- app.py
+++ app.py
@@ -171,4 +171,4 @@ def predict_disease():
 # RUN SERVER (IMPORTANT CHANGE HERE)
 # ======================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
